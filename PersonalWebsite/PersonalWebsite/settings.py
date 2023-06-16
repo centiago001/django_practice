bc29775fb884d85b62f2667702503cdc395d9c0b
centiago_001 <55315949+centiago001@users.noreply.github.com>
2023-06-16 12:44:21 +0530

    Update settings.py

--- PersonalWebsite/PersonalWebsite/settings.py
+++ PersonalWebsite/PersonalWebsite/settings.py
@@ -128,8 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT='587'
-EMAIL_HOST_USER='testing8854mail@gmail.com'
-EMAIL_HOST_PASSWORD='lzkiltqjyobsxpqh'
-EMAIL_USE_TLS= True
+EMAIL_HOST=
+EMAIL_PORT=
+EMAIL_HOST_USER='
+EMAIL_HOST_PASSWORD=
+EMAIL_USE_TLS= 
